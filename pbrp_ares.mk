17da4c73d5787cc157886eec360a8a33b5553cab
Axlorin° <154013417+axl277@users.noreply.github.com>
2026-04-17 22:57:33 +0700

    Update pbrp_ares.mk

--- pbrp_ares.mk
+++ pbrp_ares.mk
@@ -15,16 +15,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Device-specific configuration
-$(call inherit-product, device/xiaomi/ares/device.mk)
+$(call inherit-product, device/xiaomi/chopin/device.mk)
 
 # Device identity
-PRODUCT_DEVICE := ares
-PRODUCT_NAME := pbrp_ares
+PRODUCT_DEVICE := chopin
+PRODUCT_NAME := pbrp_chopin
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := M2104K10I
+PRODUCT_MODEL := 21061110AG
 PRODUCT_MANUFACTURER := Xiaomi
 
 # PBRP-specific flags
-PBRP_MAINTAINER := EronX
-PBRP_DEVICE := ares
+PBRP_MAINTAINER := Axlorin
+PBRP_DEVICE := chopin
 PBRP_VERSION := 4.0
