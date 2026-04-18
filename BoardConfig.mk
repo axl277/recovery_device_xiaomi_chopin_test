be65085860e5bb053a4546faeaee0816f2172129
Axlorin° <154013417+axl277@users.noreply.github.com>
2026-04-18 07:41:56 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,6 +169,7 @@ TW_INCLUDE_REPACKTOOLS := true
 TW_INCLUDE_RESETPROP := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_INCLUDE_NTFS_3G := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_SCREEN_BLANK_ON_BOOT:= true
 TW_THEME := portrait_hdpi
